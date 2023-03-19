a2f4128e4945ea3e2af24dadc5a472a6637c85a3
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-19 21:09:57 +0800

    reduce the padding bottom

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
                 <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden">
                     {open ? <GrClose /> : <GiHamburgerMenu />}
                 </div>
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white
+                <ul className={`md:flex md:items-center md:pb-0 pb-7 absolute md:static bg-white
                 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500
                 ease-in ${open ? 'top-12 opacity-100' : 'top-[-490px] md:opacity-100 opacity-0'}`}>
                     {

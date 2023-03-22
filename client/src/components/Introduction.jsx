f502d1048676d68dcfa7769e2852daf0d137d967
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-22 20:06:18 +0800

    Add custom color and font, add design to introduction

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -8,8 +8,8 @@ const Introduction = () => {
         <div className="px-4">
             {/* <div className="m-auto lg:w-[1150px] lg:h-[400px] md:w-[800px] md:h-[300px] 
             w-100 h-96 bg-gray-200 rounded-md mt-28 grid md:grid-cols-2 grid-cols-1"> */}
-                <div className="m-auto w-100 lg:h-[420px] md:h-[350px] h-[300px] bg-blue-300 rounded-md mt-28 grid md:grid-cols-2 grid-cols-1">
-                <div className="text-white bg-red-400 flex flex-col gap-3 justify-center lg:pl-10 md:pl-5 pl-5 order-last md:order-1">
+                <div className="m-auto w-100 lg:h-[420px] md:h-[350px] h-[300px] bg-customBlue rounded-md mt-28 grid md:grid-cols-2 grid-cols-1">
+                <div className="text-white  flex flex-col gap-3 justify-center lg:pl-10 md:pl-5 pl-5 order-last md:order-1">
                     <div>
                         <h2 className="lg:text-5xl text-4xl font-bold">Hi, I'm</h2>
                         <h2 className="lg:text-5xl text-4xl font-bold">Lord Miacky Keiji</h2>
@@ -30,8 +30,15 @@ const Introduction = () => {
                     </div>
                     
                 </div>
-                <div className="bg-green-400 order-2">
-
+                <div className=" order-2 font-Rubiks text-white text-6xl flex flex-nowrap flex-col truncate relative">
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
                 </div>
                 
             </div>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rubiks: ['Rubik Vinyl', 'cursive'],
+      },
+      colors: {
+        customBlue: "#96B9D0",
+      },
+    },
   },
   plugins: [],
 }

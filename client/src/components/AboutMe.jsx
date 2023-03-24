b724ac0b716c6cf8335e8c2905a10f61e5b27a23
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-25 01:11:02 +0800

    change the color of some elements

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import background from "../images/graduationDesign.png"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const AboutMe = () => {
     return(
         <div className="px-4">
             <div className=" w-100 md:h-[500px] h-[650px] md:mt-10 mt-1 mb-24 grid md:grid-cols-3 grid-cols-1">
-                <div className=" md:flex md:justify-center hidden">
+                <div className=" md:flex md:justify-center hidden" data-aos="fade-right" data-aos-duration="1500">
                 <div style={{
                         backgroundImage: `url("${background}")`
-                    }} className="lg:w-[18rem] md:w-[17rem] md:h-[23rem] lg:h-[25rem] hidden md:block bg-cover bg-center self-center"></div>
+                    }} className="lg:w-[18rem] md:w-[17rem] md:h-[23rem] lg:h-[25rem] hidden md:block bg-cover bg-center self-center" 
+                    ></div>
                 </div>
-                <div className="p-7 md:col-span-2">
+                <div className="p-7 md:col-span-2" data-aos="fade-left" data-aos-duration="1500">
                     <div>
                         <h3 className="text-customBlue font-bold text-xl text-center md:text-left">Discover</h3>
-                        <h2 className="text-4xl text-center md:text-left font-bold underline decoration-customlightBlue decoration-[4px] underline-offset-[9px]">About Me</h2>
+                        <h2 className="text-4xl text-center md:text-left font-bold underline
+                         decoration-customBlue decoration-[4px] underline-offset-[9px]">About Me</h2>
                         <p className="text-justify mt-5">Highly motivated and detail-oriented ICT student with strong communication and analytical skills. Proven ability
                         to learn new technologies quickly, ability to work well in a team environment, and strong problem-solving skills.
                         Passionate about technology and eager to contribute to the industry through a challenging and dynamic work
--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -5,16 +5,19 @@ import { BsFacebook } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { TypeAnimation } from 'react-type-animation';
 import 'animate.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Introduction = () => {
     const [animate, setAnimate] = useState(false);
     return(
-        <div className="px-4">
+        <div className="px-4 mt-28">
                 {/* parent of two grids */}
-                <div className=" w-100 lg:h-[450px] md:h-[350px] h-[650px] bg-customBlue rounded-md mt-28 grid md:grid-cols-2 grid-cols-1">
+                <div className=" w-100 lg:h-[450px] md:h-[350px] h-[650px] bg-customBlue rounded-md grid md:grid-cols-2 grid-cols-1">
 
                 {/* grid cols 1 */}
-                <div className="text-white  flex flex-col gap-3 justify-center lg:pl-10 py-5 pl-5 order-last md:order-1">
+                <div className="text-white  flex flex-col gap-3 justify-center lg:pl-10 py-5 pl-5 order-last md:order-1" data-aos="fade-right" data-aos-duration="1500">
                     <div>
                         <h2 className="lg:text-5xl text-4xl font-bold">Hi, I'm</h2>
                         <h2 className="lg:text-5xl text-4xl font-bold">Lord Miacky Keiji</h2>
@@ -62,13 +65,13 @@ const Introduction = () => {
                 </div>
 
                 {/* grid cols 2 */}
-                <div className=" order-2 font-Rubiks text-white lg:text-7xl text-6xl flex flex-nowrap justify-center flex-col truncate relative">
+                <div className=" order-2 font-Rubiks text-white lg:text-7xl text-6xl flex flex-nowrap justify-center flex-col truncate relative" data-aos="fade-left" data-aos-duration="1500">
                     <div style={{
                         backgroundImage: `url("${background}")`
                     }} className="lg:w-96 md:h-full md:w-80 w-96 h-full bg-cover bg-center absolute self-center"></div>
                     <div onMouseEnter={() => setAnimate(true)} onMouseLeave={() => setAnimate(false)} className={`lg:w-28 lg:h-28 md:w-24 md:h-24 w-28 h-28 rounded-full absolute bg-white self-center lg:mr-72 md:mr-60 mr-44 mb-28 lg:text-xl md:text-lg text-xl text-customBlue 
                     flex items-center justify-center font-[Poppins] font-bold text-center -rotate-[17deg] cursor-pointer ${animate ? "animate__animated animate__shakeY": ""}`}><h2>Hire me!</h2></div>
-                    <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
+                    <div><h2 >LORD MIACKY KEIJI PLATA</h2></div>
                     <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
                     <div><h2>LORD MIACKY KEIJI PLATA</h2></div>
                     <div><h2>LORD MIACKY KEIJI PLATA</h2></div>

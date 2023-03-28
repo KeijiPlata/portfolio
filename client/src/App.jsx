982ca8dddc3825af005a53fdc2e281220edb1057
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-28 17:02:19 +0800

    Add footer and fix the responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Introduction from './components/Introduction'
 import LatestProject from './components/LatestProjects'
 import Nav from './components/Nav'
 import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <AboutMe />
       <Skills />
       <LatestProject />
+      <Footer />
     </div>
   )
 }

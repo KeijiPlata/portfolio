bc6de2a990bc8ae43c0aac45f26d377f9162f2ee
KeijiPlata <lordmiackykeijip@gmail.com>
2023-03-29 00:57:17 +0800

    adding dist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
   <BrowserRouter>
      <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio/" element={<Home />} />
       </Routes>
   </BrowserRouter>
   )

d029dd67015c19f94a57395f9681573a094204c4
KeijiPlata <lordmiackykeijip@gmail.com>
2023-12-21 18:20:22 +0800

    add new component expertise, and put it in pages

---
+++ client/src/components/Expertise.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+const Expertise = () => {
+    return(
+        <div className='bg-blue-500'>
+            <h1>hello world</h1>
+        </div>
+    )
+}
+
+export default Expertise;

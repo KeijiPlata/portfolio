dbfb9bfabd2a45792d144bccc45f02620e869cdf
KeijiPlata <lordmiackykeijip@gmail.com>
2023-12-24 02:24:32 +0800

    add title for the expertise

--- client/src/components/Expertise.jsx
+++ client/src/components/Expertise.jsx
@@ -6,8 +6,12 @@ AOS.init();
 
 const Expertise = () => {
     return(
-        <div className='bg-blue-500'>
-            <h1>hello world</h1>
+        <div className='px-4 mb-20' id="experties">
+             <h3 className="text-center text-customBlue font-bold text-xl" 
+             data-aos="fade-up" data-aos-duration="1500">Experties</h3>
+            <h2 className="text-center text-4xl font-bold underline decoration-customBlue
+             decoration-4 underline-offset-[9px]" 
+             data-aos="fade-up" data-aos-duration="1500">What I do?</h2>
         </div>
     )
 }

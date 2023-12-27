1c80c877f481b4fe04c99830062d10082f9de721
KeijiPlata <lordmiackykeijip@gmail.com>
2023-12-27 16:50:06 +0800

    add new component and add it to home page

---
+++ client/src/components/Certificates.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+const Certificates = () => {
+    return(
+        <div className="bg-blue-500">
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Certificates;

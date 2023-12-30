29847437cb3526bb88b6a3f67604cf4771543b3d
KeijiPlata <lordmiackykeijip@gmail.com>
2023-12-30 23:29:04 +0800

    update footer, update information

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,80 +1,74 @@
 import React from "react";
 import { GiMineralHeart } from "react-icons/gi";
-import background from "../images/keijiCutdesign.png"
+import background from "../images/keijiCutdesign.png";
 import { BsGithub } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
-import { FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import { FaMobileAlt, FaEnvelope, FaCoffee } from "react-icons/fa";
 import { BsFillTelephoneFill } from "react-icons/bs";
 
 const Footer = () => {
-    const data = [
-        {logo: <FaMobileAlt />, info: "09980752227"},
-        {logo: <FaEnvelope />, info: "lordmiackykeijip"},
-        {logo: <BsFillTelephoneFill />, info: "89971255"},
-        
-    ]
-    return(
-        <div className="bg-customBlue">
-        <div className="grid lg:grid-cols-4 grid-cols-2">
-           
-            <div className="p-4 flex justify-center items-center">
-                    <div>
-                    <div style={{
-                        backgroundImage: `url("${background}")`
-                    }} className=" w-44 h-56 bg-cover bg-center self-center"></div>
-                       
-                    </div>
-                </div>
-            <div className=" text-white p-4 flex flex-col justify-center">
-                <div className="flex items-center gap-1 text-2xl
-                    font-bold">
-                    <div>
-                        <GiMineralHeart />
-                    </div>
-                    <div className=" font-[Poppins]">
-                        <h2>Plata.</h2>
-                    </div>
-                    </div>
-                    <p className="text-justify text-sm md:text-base">This is my first portfolio website. Thank you for visiting!</p>
-                </div>
-                <div className=" text-white p-4 flex flex-col justify-start md:justify-center lg:items-center items-center">
-                <div>
-                    <h2 className="text-lg font-bold">Contact Me</h2>
-                        <ul>
-                            
-                            {
-                                data.map((data)=>(
-                                    <li key={data.info} className="flex items-center gap-1 text-sm md:text-base">
-                                        <span>{data.logo}</span>
-                                        {data.info}
-                                        </li>
-                                )) 
-                            }
-                        </ul>
-                </div>
-            </div>
-            <div className=" text-white p-4 flex flex-col justify-start md:justify-center items-center">
-                <div>
-                    <h2 className="text-lg font-bold mb-2">Follow Me</h2>
-                    <div className="flex flex-row md:gap-3 gap-2 items-center text-3xl">
-                            <a href="https://github.com/KeijiPlata" target="_blank" className="
-                            duration-500 hover:text-customlightBlue"><BsGithub /></a>
-                            <a href="https://www.facebook.com/lordmiackykeiji" target="_blank" className="
-                            duration-500 hover:text-customlightBlue"><BsFacebook /></a>
-                            <a href="https://www.linkedin.com/in/lord-miacky-keiji-plata/" target="_blank" className="
-                            duration-500 hover:text-customlightBlue"><BsLinkedin /></a>
-                        </div>  
-                </div>
-            </div>
-           
-        </div>
-        <div className="p-2">
-        <hr className="mx-auto my-2 bg-gray-100 border-1 rounded md:my-2"></hr>
-            <h2 className="text-center text-white">Copyright © 2023 Plata</h2>
-        </div>
-        </div>
-    )
-}
+  const data = [
+    { logo: <FaMobileAlt />, info: "09980752227", title: "Mobile" },
+    {
+      logo: <FaEnvelope />,
+      info: "lordmiackykeijip@gmail.com",
+      title: "Email",
+    },
+    { logo: <BsFillTelephoneFill />, info: "89971255", title: "Telephone" },
+    { logo: <FaCoffee/>, info: "Cainta, Rizal, Philippines", title: "Meet me" },
 
-export default Footer
+  ];
+  return (
+    <div className="bg-customBlue">
+      <div className="grid md:grid-cols-2 grid-cols-1 p-10 gap-3">
+        <div className="flex gap-2 justify-center items-center">
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-1">
+              <div className="bg-white w-28 rounded-lg flex justify-center items-center">
+                <h3 className="text-customBlue font-Poppins">Get in Touch</h3>
+              </div>
+
+              <h2 className="text-white font-poppins text-5xl font-bold">
+                Talk Or Meet With Me
+              </h2>
+            </div>
+            <div className="flex flex-col gap-3">
+              {data.map((item) => (
+                <div className="flex flex-row gap-6">
+                  <div className="w-16 h-16 rounded-lg bg-white flex justify-center items-center">
+                    <div className="text-4xl text-customBlue">{item.logo}</div>
+                  </div>
+                  <div className="flex flex-col">
+                    {/* <div className="bg-white w-28 rounded-lg flex justify-center items-center"> */}
+                      <h3 className="text-white font-bold text-lg font-Poppins ">
+                        {item.title}
+                      </h3>
+                    {/* </div> */}
+
+                    <h2 className="text-white font-Poppins text-3xl ">
+                      {item.info}
+                    </h2>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 items-center justify-center">
+          <div className="flex flex-col gap-1">
+            <div className="bg-white w-36 rounded-lg flex justify-center items-center">
+              <h3 className="text-customBlue font-Poppins">Estimate Project</h3>
+            </div>
+
+            <h2 className="text-white font-poppins text-4xl font-bold">
+              Let Me Know Here
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

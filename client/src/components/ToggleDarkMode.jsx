4f8f784c9d6f9cd7294b7f6ff1068da6a727e230
KeijiPlata <lordmiackykeijip@gmail.com>
2024-02-20 21:00:56 +0800

    chore: add custom colors to tailwind config, feature: add fixed button for toggle dark to light

---
+++ client/src/components/ToggleDarkMode.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function ToggleDarkMode() {
+  return (
+    <button className="shadow-md w-16 h-16 fixed bottom-16 right-16 z-50 rounded-full">
+        Hello
+    </button>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,11 +8,13 @@ import Footer from "../components/Footer";
 import Expertise from "../components/Expertise";
 import Certificates from "../components/Certificates";
 import Experience from "../components/Experience";
+import ToggleDarkMode from "../components/ToggleDarkMode";
 
 function Home() {
     return (
         <div>
             <Nav />
+            <ToggleDarkMode/>
             <Introduction />
             <AboutMe />
             <Skills />
